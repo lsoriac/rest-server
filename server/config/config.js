10983c05709da2ab6c61a2a480c926a261bfd697
Santiago Soria <lsoriac@est.ups.edu.ec>
2020-07-10 10:25:54 -0500

    Actualización

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,26 @@
-//=======================
-//========PUERTO=========
-//=======================
-//configuramos el puerto
+const cred = require('./credenciales')
+    //=======================
+    //========PUERTO=========
+    //=======================
+    //configuramos el puerto
 process.env.PORT = process.env.PORT || 3000
 
 //=======================
 //========ENTORNO========
 //=======================
 
-process.env.NODE_ENV = process.env.NODE_DEF || 'dev'
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 //=======================
 //=====BASE DE DATOS=====
 //=======================
 
 let urlBD
-    /*
-    if (process.env.NODE_ENV === 'dev') {
-        urlBD = 'mongodb: //localhost:27017/cafe'
 
-    } else {*/
-urlBD = 'mongodb+srv://cafe_user:cafe_user123@cluster0.qartq.mongodb.net/cafe'
-    //}
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe'
+
+    //} else {
+    urlBD = cred
+}
 process.env.urlBD = urlBD
